9fff4f7812e6c0df73c180fbcfe77169cf3a9138
azsel27 <jseligma@andrew.cmu.edu>
2026-03-01 16:56:10 -0500

    input file processing

--- cooccurrence.py
+++ cooccurrence.py
@@ -1,26 +1,69 @@
-#OUTLINE
+import argparse
+import csv
+import sys
 
-#process anchor word dataset
-    #extract word_index and word
-    #create hashmap of word to word_index
+def parse_tsv(filename):
+    data = []
+    # Open the file and specify the tab delimiter
+    with open(filename, newline='', encoding='utf-8') as tsv:
+        reader = csv.reader(tsv, delimiter='\t')
+        for row in reader:
+            data.append(row)
+    return data
 
-#process post dataset
-    #hashmap, key = user hash, value = post text
-    #for each post, create a list for each anchor word
-    #count occurences of anchor words in a post in the list
-    #create per post co-occurence matrix (how?)
+def get_anchor_words(anchor_fname):
+    try:
+        parsed_data = parse_tsv(anchor_fname)
+    except FileNotFoundError:
+        print(f"The file '{anchor_fname}' was not found.")
+        sys.exit()
+    
+    #maps word key to word_index value
+    anchor_words = {}
+    #skip first row because it is labels
+    for row in parsed_data[1:]:
+        index = int(row[0])
+        word = row[2]
+        anchor_words[word] = index
+    
+    return anchor_words
 
-#create hashmap from user hash to matrix
 
-#for each post
-    #create_or_get(user hash) matrix  m
-    #add counts of post matrix to m
+def get_posts(post_fname):
+    try:
+        parsed_data = parse_tsv(post_fname)
+    except FileNotFoundError:
+        print(f"The file '{post_fname}' was not found.")
+        sys.exit()
 
-#for each user
-    #write matrix to output file 
+    posts = {}
+    for row in parsed_data:
+        user_hash = row[0]
+        post_content = row[1]
+        posts[user_hash] = post_content
+    
+    return posts
 
 def main():
-    print("Hello world")
+    parser = argparse.ArgumentParser(description='small script to create co-occurence matrices per user given proper datasets')
+    parser.add_argument('anchor', type=str, help='The file name the anchor words')
+    parser.add_argument('posts', type=str, help="The file name of the posts")
+    parser.add_argument('--out', type=str, help="The prefix of the output file")
+    
+    args = parser.parse_args()
+
+    #key anchor word, value index
+    anchor_words = get_anchor_words(args.anchor)
+
+    #key user hash, value post text
+    posts = get_posts(args.posts)
+
+    # print(f"Anchor: {args.anchor}")
+    # print(f"Posts: {args.posts}")
+    # print(f"Out: {args.out}")
+
+
+
 
 if __name__ == "__main__":
     main()
